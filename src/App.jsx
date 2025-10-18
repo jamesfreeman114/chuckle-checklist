5bd2ab2d14bde7d58e40fbaa54f412b92eb1cd9c
James Freeman <jamesfreeman114@gmail.com>
2025-10-18 14:13:04 -0500

    Added Joke Input Field

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react"
+import { saveJoke } from "./services/jokeService"
 import "./App.css"
 
 export const App = () => {
-  return <div>Hello World!</div>
+
+  const [userInput, setUserInput] = useState([])
+ 
+
+  return ( <div className="app-container" >
+              <div className="app-heading">
+                <h1 className="app-heading-text">Chuckle Checklist</h1>
+                <div className="joke-add-form">
+                  <h2>Add Joke</h2>
+                      <input
+                        className="joke-input"
+                        type="text"
+                        placeholder="New One Liner"
+                        onChange={(event) => {
+                            setUserInput(event.target.value)
+                            // What's the value of event?
+                          }}
+                      />
+                      <button
+                        className="joke-input-submit"
+                        placeholder="Add"
+                        onClick={() => {saveJoke(userInput)}}
+                       >Add</button>
+
+                     
+                </div>
+              </div>
+          </div>
+  
+  )
+
+  
 }
---
+++ src/services/jokeService.js
@@ -0,0 +1,25 @@
+export const getAllJokes = () => {
+    return fetch(`http://localhost:8088/jokes`).then(res => res.json())
+}
+
+
+
+
+export const saveJoke = async (userInput) => {
+
+    const transientState = {
+    text: userInput,
+    told: false
+}
+
+
+    const postJoke = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json"    
+        },
+        body: JSON.stringify(transientState)
+    }
+
+    const response = await fetch(`http://localhost:8088/jokes`, postJoke)
+}
